4309b74209496690c6b10f337cb74bc476866ca9
Stephane Bellity <sbellity@gmail.com>
2015-08-31 19:18:44 +0200

    footers

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,4 +1,7 @@
-import { Sections } from 'hull-login';
+import React from 'react';
+import { Sections, Components } from 'hull-login';
+
+let { TranslatedMessage } = Components;
 
 const sectionFooter = {
   position: 'absolute',
@@ -34,19 +37,60 @@ class signUp extends Sections.signUp {
 
 class resetPassword extends Sections.resetPassword {
   renderHeader() {}
+  renderFooter(styles) {
+    return <div style={sectionFooter}>
+      <p style={styles.sectionText}>
+        <TranslatedMessage tag='a'
+          href='#'
+          onClick={this.props.activateLogInSection}
+          message='reset password switch to log-in link' />
+      </p>
+    </div>;
+  }
 }
 
 
 class showProfile extends Sections.showProfile {
-
+  renderHeader() {}
+  renderFooter(styles) {
+    return <div style={sectionFooter}>
+      <p style={styles.sectionText}>
+        <TranslatedMessage tag='a'
+          href='#'
+          onClick={this.props.activateEditProfileSection}
+          message='view profile switch to edit profile link' />
+      </p>
+    </div>;
+  }
 }
 
 
 class editProfile extends Sections.editProfile {
+  renderHeader() {}
+  renderFooter(styles) {
+    let message;
+    let handleClick;
+    if (this.props.formIsSubmitted || !this.props.hasForm) {
+      message = 'edit profile cancel button';
+      handleClick = this.props.activateShowProfileSection;
+    } else {
+      message = 'edit profile cancel button when profile incomplete';
+      handleClick = this.props.hideDialog;
+    }
+    return <div style={sectionFooter}>
+      <p style={styles.sectionText}>
+        <a href='javascript: void 0;' onClick={handleClick}>
+          <TranslatedMessage tag='p' style={styles.sectionText} message={message} />
+        </a>
+      </p>
+    </div>;
+  }
+
 }
 
 
 class thanks extends Sections.thanks {
+  renderHeader() {}
 }
 
 
