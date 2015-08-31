d1257cfa0257e9c2f1f84a709005a7844a04584d
Stephane Bellity <sbellity@gmail.com>
2015-08-31 17:30:50 +0200

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-'use strict';
+import React from 'react';
+import { Core } from 'hull-login';
+import { setSettings } from './styles/settings';
+import Main from './main';
 
-import bootstrap from './bootstrap';
 
 function onReady(hull, user, platform, organization) {
-  let d = platform.deployments[0];
-  d.organization = organization;
+  let deployment = platform.deployments[0];
+  deployment.organization = organization;
 
-  bootstrap(document.querySelector('#ship'), d);
+  let element = document.querySelector('#ship');
+
+
+  let engine = new Core.Engine(deployment);
+
+  Core.I18n.setTranslations(deployment.ship.translations);
+
+  const shipSettings = deployment.ship.settings;
+  setSettings({
+    primaryColor: shipSettings.primary_color,
+    textColor: shipSettings.text_color,
+    linkColor: shipSettings.link_color,
+    defaultBorderRadius: shipSettings.button_border_radius,
+    mediumBorderRadius: shipSettings.overlay_border_radius
+  });
+
+  React.render(<Main engine={engine} actions={engine.getActions()} />, element);
 }
 
 Hull.ready(onReady);
